6b5ed8668c4b449b61b2f450956e78b948a5e995
Piyush khare <piyushkhare671@gmail.com>
2024-10-01 15:10:58 +0530

    update  pages

---
+++ src/app/(routes)/aboutus/page.tsx
@@ -0,0 +1,7 @@
+'use client'
+ const AboutUs = () => {
+    return(
+         <div>   here  would be our About us page which has benn assign to anuska  </div>
+    )
+}
+export default AboutUs
---
+++ src/app/(routes)/contactus/page.tsx
@@ -0,0 +1,7 @@
+'use client'
+ const Contactus = () => {
+    return(
+         <div>   here  would be our Contact us page which has benn assign to anuska  </div>
+    )
+}
+export default Contactus
