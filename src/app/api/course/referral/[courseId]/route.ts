7919c3eb0d7f90ff9cc96bcc2b9db608397f82f0
YooShijin <a7mitk000@gmail.com>
2024-10-10 02:25:54 +0530

    commented referral

--- src/app/api/course/referral/[courseId]/route.ts
+++ src/app/api/course/referral/[courseId]/route.ts
@@ -1,15 +1,15 @@
-import { NextRequest, NextResponse } from "next/server";
-import Referral from "@/models/Referal";
-import connectToMongoDB from "@/lib/mognodb";
+// import { NextRequest, NextResponse } from "next/server";
+// import Referral from "@/models/Referal";
+// import connectToMongoDB from "@/lib/mognodb";
 
-export async function POST(
-  req: NextRequest,
-  res: NextResponse,
-  params: { params: { courseId: string } }
-) {
-  await connectToMongoDB();
-  const { courseId } = params.params;
-  if (!course) {
-    return NextResponse.json({ message: "Course not found" }, { status: 404 });
-  }
-}
+// export async function POST(
+//   req: NextRequest,
+//   res: NextResponse,
+//   params: { params: { courseId: string } }
+// ) {
+//   await connectToMongoDB();
+//   const { courseId } = params.params;
+//   if (!course) {
+//     return NextResponse.json({ message: "Course not found" }, { status: 404 });
+//   }
+// }
